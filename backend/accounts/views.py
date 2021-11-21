c8b1ca63396193c0acae190a86f464cbecda9e96
george onen <geonhib@gmail.com>
2021-11-21 21:42:59 +0300

    Initial template integration.

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -4,3 +4,12 @@ from django.shortcuts import render
 def index(request):
     context = {}
     return render(request, "base.html", context)
+
+
+def error_404(request, exception):
+        data = {}
+        return render(request,'400.html', status=404)
+
+def error_500(request):
+        data = {}
+        return render(request,'500.html', status=500) 
--- backend/sitestravel/urls.py
+++ backend/sitestravel/urls.py
@@ -27,5 +27,5 @@ if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT) 
 
-# handler404 = 'accounts.views.error_404'
-# handler500 = 'accounts.views.error_500'
+handler404 = 'accounts.views.error_404'
+handler500 = 'accounts.views.error_500'
